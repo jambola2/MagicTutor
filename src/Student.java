fd889a15d3c7a80de4842ff2703a644f305eb71d
jambola2 <jambola2@users.noreply.github.com>
2014-11-29 11:25:24 +0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,9 @@
 public class Student extends User{
-
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String passwordHash;
+	
 	public Student(String f, String l, String u, String p) {
 		super(f, l, u, p);
 	}
