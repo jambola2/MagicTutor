464c5618c757105ce63daa7ddd6b64b82cd6b419
venkatesh <venkatesh@venkatesh-pc>
2014-11-29 11:43:14 +0400

    Fix indentation

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,11 @@
 public class Student extends User{
-	private String firstName;
-	private String lastName;
-	private String userName;
-	private String passwordHash;
+  private String firstName;
+  private String lastName;
+  private String userName;
+  private String passwordHash;
 	
-	public Student(String f, String l, String u, String p) {
-		super(f, l, u, p);
-	}
+  public Student(String f, String l, String u, String p) {
+	super(f, l, u, p);
+  }
   
 }
